372e97a6610ee2ec0ff5937337e7b59b56655a85
Harish Raja Selvan <98503022+Harish-Gits@users.noreply.github.com>
2024-07-19 22:00:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove character/remove-character.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String string1 = read.readLine();
+            String string2 = read.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.removeChars(string1,string2) );
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String removeChars(String string1, String string2)
+    {
+        Set<Character> set=new HashSet<>();
+        for(char c:string2.toCharArray())
+            set.add(c);
+        StringBuilder sb=new StringBuilder();
+        for(char c:string1.toCharArray())
+        {
+            if(!set.contains(c))
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+}
