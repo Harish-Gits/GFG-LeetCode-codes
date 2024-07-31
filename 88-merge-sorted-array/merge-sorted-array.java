e88f255ccc019717a2bbad1d9505103029954a5f
Harish Raja Selvan <98503022+Harish-Gits@users.noreply.github.com>
2024-07-31 12:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (35.94%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        // Iterate from the end of both arrays
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+
+        // If there are remaining elements in nums2, copy them
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+    }
+}
