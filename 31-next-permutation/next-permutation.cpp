9ab32e742a716a2a58a020d954fb7eca20d5094a
Harish Raja Selvan <98503022+Harish-Gits@users.noreply.github.com>
2024-09-08 21:45:13 +0530

    Time: 2 ms (67.28%) | Memory: 14.8 MB (80.71%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void nextPermutation(std::vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1) return;
+
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            --i;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                --j;
+            }
+
+            std::swap(nums[i], nums[j]);
+        }
+
+        std::reverse(nums.begin() + i + 1, nums.end());
+    }
+};
