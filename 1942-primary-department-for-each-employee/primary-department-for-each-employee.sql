e0f9e58d275b74d01ecf7c2f93f11dc8a77276f3
Harish Raja Selvan <98503022+Harish-Gits@users.noreply.github.com>
2024-09-11 12:25:14 +0530

    Time: 480 ms (81.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT employee_id,department_id
+FROM Employee
+WHERE primary_flag='Y' OR employee_id NOT IN (SELECT employee_id FROM Employee WHERE primary_flag='Y')
