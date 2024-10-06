d15c387e76eacb0491070287f6a0d7a8914ee339
Harish Raja Selvan <98503022+Harish-Gits@users.noreply.github.com>
2024-10-06 12:36:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.java
@@ -0,0 +1,163 @@
+//{ Driver Code Starts
+
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Gfg {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            int k = Integer.parseInt(br.readLine());
+            Node[] pre = new Node[1];
+            Node[] suc = new Node[1];
+            Solution.findPreSuc(root, pre, suc, k);
+            if (pre[0] != null) {
+                System.out.print(pre[0].data + " ");
+            } else {
+                System.out.print("-1 ");
+            }
+            if (suc[0] != null) {
+                System.out.println(suc[0].data);
+            } else {
+                System.out.println("-1 ");
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    
+    
+    public static Node findPre(Node root,int key)
+        {
+            Node ans=null;
+            while(root!=null)
+            {
+                if(root.data>=key)
+                {
+                    root=root.left;
+                }
+                else
+                {
+                    ans=root;
+                    root=root.right;
+                }
+            }
+            return ans;
+        }
+        
+        
+    public static Node findSuc(Node root,int key)
+        {
+            Node ans=null;
+            while(root!=null)
+            {
+                if(root.data<=key)
+                {
+                    root=root.right;
+                }
+                else
+                {
+                    ans=root;
+                    root=root.left;
+                }
+            }
+            return ans;
+        }
+    
+    public static void findPreSuc(Node root, Node[] pre, Node[] suc, int key) {
+        // code here.
+        
+        pre[0]=findPre(root,key);
+        suc[0]=findSuc(root,key);
+    
+        // update pre[0] with the predecessor of the key
+        // update suc[0] with the successor of the key
+    }
+}
